3134248233ddb452077d092b76339966bdec5543
Stefan <langestefan@msn.com>
2022-09-07 12:30:37 +0200

    Add week 1 - linear least squares

--- practicals/code/linear_regression.py
+++ practicals/code/linear_regression.py
@@ -16,3 +16,21 @@ def lsq(X, y):
     beta = np.dot(np.linalg.inv(np.dot(X.T, X)), np.dot(X.T, y))
 
     return beta
+
+
+def do_lsq_inference(X, y, beta):
+    """
+    Perform inference on the least squares linear regression
+    :param X: Input data matrix
+    :param y: Target vector
+    :param beta: Estimated coefficient vector for the linear regression
+    :return: Estimated variance of the coefficients
+    """
+    # y_pred
+    print(f'shapes: X {X.shape}, beta {beta.shape}')
+    y_pred = np.matmul(X, beta).squeeze()
+
+    # mse
+    mse = np.mean((y - y_pred) ** 2)
+
+    return y_pred, mse
